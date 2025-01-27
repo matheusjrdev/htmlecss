5f48f7da92ed185b7e82e039c28f39d34ce97ae3
Matheus Dev <https://github.com/matheusjrdev>
2025-01-27 05:44:43 -0300

    Começando minha aprendizagem

---
+++ Formularios/nota.js
@@ -0,0 +1,18 @@
+ /*var nota1 = Number(window.prompt('Digite primeira nota '));
+
+var nota2 = Number(window.prompt('Digite a Segunda nota '))
+
+function media() {
+    var soma = (nota1 + nota2) / 2
+    if (soma >= 7)
+        console.log("Aprovado")
+    else
+        console.log("Reprovado")
+
+
+        console.log ('a primeira nota é '+ nota1 + ' A segunda nota é ' + nota2 + ' e a media entre ela é ' + soma );
+}
+
+
+
+media (); */
